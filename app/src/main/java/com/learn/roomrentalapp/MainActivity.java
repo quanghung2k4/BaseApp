fa62cef1d730d41c17fcd574ea9778ac73e7b217
quanghung2k4 <quanghungbg2021@gmail.com>
2026-03-13 10:25:28 +0700

    update activity

--- app/src/main/java/com/learn/roomrentalapp/MainActivity.java
+++ app/src/main/java/com/learn/roomrentalapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.learn.roomrentalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvRooms;
+    private FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initViews();
+        setupRecyclerView();
+
+        fabAdd.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, AddRoomActivity.class);
+//            startActivity(intent);
         });
+    }
+
+    private void initViews() {
+        rvRooms = findViewById(R.id.rvRooms);
+        fabAdd = findViewById(R.id.fabAdd);
+    }
+
+    private void setupRecyclerView() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 }
